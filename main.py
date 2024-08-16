988cdf28ea2cab8f62e9b6864d903d9c25f3a185
Martin <p0m3l0pack@gmail.com>
2024-08-16 10:57:31 +0200

    add sranec
    haha

--- main.py
+++ main.py
@@ -3,7 +3,13 @@ from flask import request
 from flask import jsonify
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import psycopg2
+
+#connect to database
+connection = psycopg2.connect(host='localhost', port='5432', database='task_manager', user='postgres', password='57Baf1a8?')
 
 
+
+#setup slack
 app = Flask(__name__)
 
